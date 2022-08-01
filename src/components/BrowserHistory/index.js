2f2b5219c7cecfe2e4b2d2a25092c8f26c843824
venky <venky9177467937@gmail.com>
2022-08-01 06:55:08 +0000

    history4

--- src/components/BrowserHistory/index.js
+++ src/components/BrowserHistory/index.js
@@ -3,30 +3,60 @@ import './index.css'
 import List from '../List'
 
 class BrowserHistory extends Component {
-  // state = {searchInput: 0}
+  state = {searchInput: ''}
+
+  searchingHistory = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  // change = id => {
+  // const {HistoryDetails} = this.state
+  // const afterDeleteHistoryList = HistoryDetails.filter(each => each.id !== id)
+  // this.setState({HistoryDetails: afterDeleteHistoryList})
+  // }
 
   render() {
-    // const {searchInput} = this.state
+    // const {initialHistoryDetails} = this.props
+    const {searchInput} = this.state
     const {initialHistoryList} = this.props
+
+    const searchResult = initialHistoryList.filter(eachList =>
+      eachList.title.toLowerCase().includes(searchInput.toLowerCase()),
+    )
 
     return (
       <div className="bg">
         <div className="searchRow">
-          <h1 className="heading">History</h1>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
+            alt="app logo"
+            className="History-Img"
+          />
+
           <div className="searchContainer">
             <img
               src="https://assets.ccbp.in/frontend/react-js/search-img.png"
-              alt="domain logo"
+              alt="search"
               className="searchImg"
             />
-            <input type="search" placeholder="search" className="input" />
+            <input
+              type="search"
+              placeholder="search"
+              className="input"
+              value={searchInput}
+              onChange={this.searchingHistory}
+            />
           </div>
         </div>
 
         <div className="history-container">
           <ul className="ul-container">
-            {initialHistoryList.map(each => (
-              <List initialHistoryDetails={each} key={each.id} />
+            {searchResult.map(each => (
+              <List
+                initialHistoryDetails={each}
+                key={each.id}
+                change={this.onchange}
+              />
             ))}
           </ul>
         </div>
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,22 +1,26 @@
 import './index.css'
 
 const List = props => {
-  const {initialHistoryDetails} = props
-  const {timeAccessed, logoUrl, title, domainUrl} = initialHistoryDetails
+  const {initialHistoryDetails, change} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = initialHistoryDetails
+  const deleteHistory = () => {
+    change(id)
+  }
   return (
     <li className="List-container">
       <div className="list-fist-part">
         <p>{timeAccessed}</p>
-        <img src={logoUrl} alt={title} className="img" />
+        <img src={logoUrl} alt="domain logo" className="img" />
         <p>{title}</p>
-        <p>{domainUrl}</p>
+        <p className="domain-name">{domainUrl}</p>
       </div>
       <div className="delete-button">
-        <button type="button">
+        <button type="button" onClick={deleteHistory}>
           <img
             src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
             alt="delete"
             className="delete-button"
+            id={id}
           />
         </button>
       </div>
